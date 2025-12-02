b1a668d346e22f8b00608d0e27c9dbfba1083850
Stuart Pineo <svpineo@gmail.com>
2025-12-02 17:20:38 +0000

    Working lambda deployment.

--- providers.tf
+++ providers.tf
@@ -1,12 +1,11 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
+ required_providers {
+   aws = {
+     source = "hashicorp/aws"
+     version = "~> 4.0"
+   }
+ }
 }
-
 provider "aws" {
-  region = "us-west-2"
+ region = "us-east-1"
 }
